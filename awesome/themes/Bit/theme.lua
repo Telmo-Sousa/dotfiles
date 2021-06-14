cdb6572af7eb4f7eb5fcdd7524d300aeb48d8957
Sousa <68487926+Telmo-Sousa@users.noreply.github.com>
2021-06-14 18:36:43 +0100

    awesome dotfiles upload

---
+++ awesome/themes/Bit/theme.lua
@@ -0,0 +1,56 @@
+-- Bit
+------------------
+
+local theme = {}
+
+theme.font          = '8-bit Operator+ 8 Bold'
+
+theme.bg_normal     = "#37395E"
+theme.bg_focus      = "#4C4f82"
+theme.bg_urgent     = "#48FFDC"
+theme.bg_minimize   = "#4C4f82"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#ECBDF6"
+theme.fg_focus      = "#F2E1F6"
+theme.fg_urgent     = "#F2E1F6"
+theme.fg_minimize   = "#F2E1F6"
+
+theme.useless_gap   = 7
+theme.border_width  = 3
+theme.border_normal = "#84748c"
+theme.border_focus  = "#547c8c"
+theme.border_marked = "#adc5c8"
+
+-- Hotkeys popup
+theme.hotkeys_bg = '#adc5c8'
+theme.hotkeys_fg = '#547cac'
+theme.hotkeys_opacity = '0.9'
+theme.hotkeys_border_width = 3
+theme.hotkeys_border_color = '#84748c'
+theme.hotkeys_modifiers_fg = '#547cac'
+theme.hotkeys_label_fg = '#1B1D27'
+theme.hotkeys_font = '8-bit Operator+ SC Bold'
+theme.hotkeys_description_font = '8-bit Operator+ SC'
+theme.hotkeys_group_margin = 20
+
+-- Wallpaper
+theme.wallpaper = '~/.config/awesome/themes/Bit/wallpaper.png'
+
+return theme
+
+--background = #4d5164
+--foreground = #a1b379
+--background = rgba(77,81,100,0.8)
+
+--highlight = #454C61
+
+--# Normal Colors
+--color0 = #547c8c
+--color1 = #b4b1a3
+--color2 = #7ca4c4
+--color3 = #adc5c8
+--color4 = #e3dbd3
+--color5 = #84748c
+--color6 = #547cac
+--color7 = #b49c94
